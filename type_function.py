2f2b7972d25c63a38cd6da480d76438cffd2a264
GRRosti <rosti.greenberg@gmail.com>
2025-04-23 17:09:52 +0000

    Changes to be committed: 	new file:   type_function.py

---
+++ type_function.py
@@ -0,0 +1,10 @@
+# 1. Create variables with the specified values
+num = 10
+string_variable = "Hello"
+boolean_variable = True
+
+# Use the type() function to determine the type of each variable and print the result
+print(f"The value of num is {num} and its type is {type(num)}")
+print(f"The value of str is {string_variable} and its type is {type(string_variable)}") # Using string_variable as per the code
+print(f"The value of bool is {boolean_variable} and its type is {type(boolean_variable)}") # Using boolean_variable as per the code
+

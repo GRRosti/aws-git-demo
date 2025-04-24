27bd4355f80f69e3850e38958771887ec817dedd
GRRosti <rosti.greenberg@gmail.com>
2025-04-24 09:28:59 +0000

    new file:   division_test.py

---
+++ division_test.py
@@ -0,0 +1,21 @@
+print ("please enter two different numbers greater than 0")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+test_range = range(a, b)
+dividers = int(input("Enter a number to check if it divides the range: "))
+list1 = []
+list2 = []
+def test_division():
+    for i in test_range:
+        if i % dividers == 0:
+            while True:
+                list1.append(i)
+                break
+        else:
+            list2.append(i)
+test_division()
+print("The numbers that are divisible by", dividers, "are:", list1)
+print("There are", len(list1), "numbers that are divisible by", dividers)
+print("The numbers that are not divisible by", dividers, "are:", list2)
+print("There are", len(list2), "numbers that are not divisible by", dividers)
+print("Thanks for playing!")
